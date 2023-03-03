59e66b44911c3332cba32dd040ea8ae85128b2d0
CasinoHe <casinohe@outlook.com>
2023-03-03 10:52:35 +0800

    fix configure and compile error on ubuntu 22.04

--- cmake/zlib.cmake
+++ cmake/zlib.cmake
@@ -4,14 +4,32 @@
 add_subdirectory(src/common/zlib)
 include_directories(${CMAKE_BINARY_DIR}/src/common/zlib)
 
-set(ZLIB_LIBRARY_DIRS ${CMAKE_BINARY_DIR}/src/common/zlib/${CMAKE_BUILD_TYPE})
+# msvc will add build_type directory to the library path
+if (MSVC)
+	set(ZLIB_LIBRARY_DIRS ${CMAKE_BINARY_DIR}/src/common/zlib/${CMAKE_BUILD_TYPE})
+else()
+	set(ZLIB_LIBRARY_DIRS ${CMAKE_BINARY_DIR}/src/common/zlib/)
+endif()
+
 message(STATUS "ZLIB_LIBRARY_DIRS: ${ZLIB_LIBRARY_DIRS}")
+
 set(ZLIB_FOUND TRUE)
 set(ZLIB_INCLUDE_DIRS ${CMAKE_BINARY_DIR}/src/common/zlib ${MULTIPLAYER_SERVER_ROOT_DIR}/common/zlib)
-set(ZLIB_LIBRARIES 
-	${ZLIB_LIBRARY_DIRS}/zlibd${CMAKE_STATIC_LIBRARY_SUFFIX}
-)
 set(ZLIB_INCLUDE_DIR ${CMAKE_BINARY_DIR}/src/common/zlib ${MULTIPLAYER_SERVER_ROOT_DIR}/common/zlib)
-set(ZLIB_LIBRARY 
-	${ZLIB_LIBRARY_DIRS}/zlibd${CMAKE_STATIC_LIBRARY_SUFFIX}
-)
+
+# there are different names for the library on different platforms
+if (MSVC)
+	set(ZLIB_LIBRARIES
+		${ZLIB_LIBRARY_DIRS}/zlibd${CMAKE_STATIC_LIBRARY_SUFFIX}
+	)
+	set(ZLIB_LIBRARY
+		${ZLIB_LIBRARY_DIRS}/zlibd${CMAKE_STATIC_LIBRARY_SUFFIX}
+	)
+else()
+	set(ZLIB_LIBRARIES
+		${ZLIB_LIBRARY_DIRS}/libz${CMAKE_STATIC_LIBRARY_SUFFIX}
+	)
+	set(ZLIB_LIBRARY
+		${ZLIB_LIBRARY_DIRS}/libz${CMAKE_STATIC_LIBRARY_SUFFIX}
+	)
+endif()
